f36d783b2995ec5596c7212afd46c0a6149c5064
Jrao01 <julianrafael1604@gmail.com>
2025-07-12 12:13:27 -0400

    1.2.2.5 (se anadio adminroute)

---
+++ routes/adminroutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {GetClients,GetProfesionales,isUserBlocked} = require('../controllers/adminController');
+
+router.get('/admin/getClients',GetClients);
+router.get('/admin/getPro',GetProfesionales);
+router.get('/admin/CheckBlocked',isUserBlocked)
+
+
+
+mopdule.exports = router;
